9f3d3694e514c4114c3969516f5a607620e21bb8
imortalplay <vinicius.pereira@facepe.br>
2019-10-13 16:44:11 -0300

    Tentativa de teste com JUnit.

---
+++ testessrc/com/ecodeup/testes/ConectionTest.java
@@ -0,0 +1,19 @@
+package com.ecodeup.testes;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ConectionTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testGetConnection() {
+		fail("Not yet implemented");
+	}
+
+}
